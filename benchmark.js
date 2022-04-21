018b6f64a583a90de54c14e2bafcf23388ea9b19
Carlos Ferraz <93772448+CarlosLF1@users.noreply.github.com>
2022-04-21 18:04:18 +0300

    Update JS logic to test

--- benchmark.js
+++ benchmark.js
@@ -6,30 +6,44 @@
 
 let questions = [
     {
-        prompt:'Arrays in JS are defined by which of the following statements?\n(a) Ordered list of values\n\
+        prompt:'Arrays in JS are defined by which of the following statements?\n\
+            (a) Ordered list of values\n\
             (b) Ordered list of objects\n\
-            (c) Ordered list of strings\n(d) Ordered list of functions',
+            (c) Ordered list of strings\n\
+            (d) Ordered list of functions',
         answer: 'a'
     },
     {
-        prompt: 'Which of the following is not js data types?\n(a) Null type\n\
-                (b) Undefined type\n(c) Number type\n(d) All of the mentioned',
+        prompt: 'Which of the following is not js data types?\n\
+                (a) Null type\n\
+                (b) Undefined type\n\
+                (c) Number type\n\
+                (d) All of the mentioned',
         answer: 'd'
     },
     {
-        prompt: 'The function and var/let are known as?\n(a) Keywords\n(b) Data types\n\
-                (c)Declaration statements\n(d) error',
+        prompt: 'The function and var/let are known as?\n\
+                (a) Keywords\n\
+                (b) Data types\n\
+                (c)Declaration statements\n\
+                (d) error',
         answer: 'c'
     },
     {
-        prompt: 'Which of the following statement is suitable to check if pattern matches the string text?\n(a) test(text\n\
-                (b) equals(pattern)\n(c) test(pattern)\n(d) text==pattern',
+        prompt: 'Which of the following statement is suitable to check if pattern matches the string text?\n\
+                (a) test(text\n\
+                (b) equals(pattern)\n\
+                (c) test(pattern)\n\
+                (d) text==pattern',
         answer: 'd'
 
     },
     {
         prompt: 'Which of the following is known as the equality operator, used to check whether two values are equal or not?\n\
-                (a) =\n(b)===\n(c)==\n(d)&&',
+                (a) =\n\
+                (b)===\n\
+                (c)==\n\
+                (d)&&',
         answer: 'c'
     }
 
@@ -48,6 +62,8 @@ for (let index = 0; index < questions.length; index++) {
     }
 }
 alert('You got' + score + '/' + questions.length);
+
+
 
 // let anotherButton = document.querrySelector("#button__container")
 
