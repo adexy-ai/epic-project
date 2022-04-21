b12d6ecb8254cea5ffc9f402a36016c3e39748d8
Aderiye <69057307+adexy-ai@users.noreply.github.com>
2022-04-21 13:25:33 +0300

    working on the js logic

---
+++ benchmark.js
@@ -0,0 +1,12 @@
+// let myHeader = document.getElementsByTagName("h1");
+// let myButton = document.getElementsByTagName("button")
+// let myQuestion = document.getElementById("footer")(need to generate a random question)
+let question = ["question1", "question2", "question 3", "question4", "question 5"]
+Math.random
+// let anotherButton = document.querrySelector("#button__container")
+
+// // document.getElementsByTagName(button).innerHTML =
+// //     "this is an example of a question" + element.innerHTML;
+
+// console.log("this is an exeample of output");
+alert("this js is working")
