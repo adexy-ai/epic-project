08a79398b0b6a4efe978b8df25704f7d9756f65b
farshadBish <farshadota@gmail.com>
2022-04-22 16:59:41 +0200

    FINAL

---
+++ welcome.js
@@ -0,0 +1,13 @@
+let rulesNode = document.getElementsByTagName('input')[0];
+let buttonNode = document.getElementById("proc");
+buttonNode.onclick = function (){
+    location.href = "Quiz.html";
+}
+rulesNode.onchange = function(){
+    if(this.checked){
+        buttonNode.disabled = false;
+    }else{
+        buttonNode.disabled = true;
+        alert('You should agree with our term of usage to proceed');
+    }
+}
