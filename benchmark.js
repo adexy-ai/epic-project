d640d2e51d83c3b3222a8eea0ee30c973b566797
Aderiye <69057307+adexy-ai@users.noreply.github.com>
2022-04-22 12:59:47 +0300

    latest update

--- benchmark.js
+++ benchmark.js
@@ -1,121 +1,125 @@
 
-// let myHeader = document.getElementsByTagName("h1");
-// let myButton = document.getElementsByTagName("button")
-// let myQuestion = document.getElementById("footer")(need to generate a random question)
-//let question = ["question1", "question2", "question 3", "question4", "question 5"]
-//Math.random
+// // let myHeader = document.getElementsByTagName("h1");
+// // let myButton = document.getElementsByTagName("button")
+// // let myQuestion = document.getElementById("footer")(need to generate a random question)
+// //let question = ["question1", "question2", "question 3", "question4", "question 5"]
+// //Math.random
 
-//let questions = [
-/*     {
-        prompt:'Arrays in JS are defined by which of the following statements?\n\
-            (a) Ordered list of values\n\
-            (b) Ordered list of objects\n\
-            (c) Ordered list of strings\n\
-            (d) Ordered list of functions',
-        answer: 'a'
-    },
-    {
-        prompt: 'Which of the following is not js data types?\n\
-                (a) Null type\n\
-                (b) Undefined type\n\
-                (c) Number type\n\
-                (d) All of the mentioned',
-        answer: 'd'
-    },
-    {
-        prompt: 'The function and var/let are known as?\n\
-                (a) Keywords\n\
-                (b) Data types\n\
-                (c)Declaration statements\n\
-                (d) error',
-        answer: 'c'
-    },
-    {
-        prompt: 'Which of the following statement is suitable to check if pattern matches the string text?\n\
-                (a) test(text\n\
-                (b) equals(pattern)\n\
-                (c) test(pattern)\n\
-                (d) text==pattern',
-        answer: 'd'
+// //let questions = [
+// /*     {
+//         prompt:'Arrays in JS are defined by which of the following statements?\n\
+//             (a) Ordered list of values\n\
+//             (b) Ordered list of objects\n\
+//             (c) Ordered list of strings\n\
+//             (d) Ordered list of functions',
+//         answer: 'a'
+//     },
+//     {
+//         prompt: 'Which of the following is not js data types?\n\
+//                 (a) Null type\n\
+//                 (b) Undefined type\n\
+//                 (c) Number type\n\
+//                 (d) All of the mentioned',
+//         answer: 'd'
+//     },
+//     {
+//         prompt: 'The function and var/let are known as?\n\
+//                 (a) Keywords\n\
+//                 (b) Data types\n\
+//                 (c)Declaration statements\n\
+//                 (d) error',
+//         answer: 'c'
+//     },
+//     {
+//         prompt: 'Which of the following statement is suitable to check if pattern matches the string text?\n\
+//                 (a) test(text\n\
+//                 (b) equals(pattern)\n\
+//                 (c) test(pattern)\n\
+//                 (d) text==pattern',
+//         answer: 'd'
 
-    },
-    {
-        prompt: 'Which of the following is known as the equality operator, used to check whether two values are equal or not?\n\
-                (a) =\n\
-                (b)===\n\
-                (c)==\n\
-                (d)&&',
-        answer: 'c'
-    }
+//     },
+//     {
+//         prompt: 'Which of the following is known as the equality operator, used to check whether two values are equal or not?\n\
+//                 (a) =\n\
+//                 (b)===\n\
+//                 (c)==\n\
+//                 (d)&&',
+//         answer: 'c'
+//     }
 
-]
-console.log(questions);
+// ]
+// console.log(questions);
 
-let score = 0;
+// let score = 0;
 
-for (let index = 0; index < questions.length; index++) {
-    let response = window.prompt(questions[index].prompt)
-    if (response == questions[index].answer) {
-        score++;
-        alert('Correct answer!');
-    } else {
-        alert('Wrong, try again!')
-    }
-}
-alert('You got' + score + '/' + questions.length); */
+// for (let index = 0; index < questions.length; index++) {
+//     let response = window.prompt(questions[index].prompt)
+//     if (response == questions[index].answer) {
+//         score++;
+//         alert('Correct answer!');
+//     } else {
+//         alert('Wrong, try again!')
+//     }
+// }
+// alert('You got' + score + '/' + questions.length); */
 
-let questions = [{
-    question: 'Arrays in JS are defined by which of the following statements?',
-    correctAnswer: 'Ordered list of values',
-    incorrectAnswers: ['Ordered list of objects','Ordered list of strings','Ordered list of functions'],
-    },
-    {
-    question: 'Which of the following is not js data types?',
-    correctAnswer: 'All of the mentioned',
-    incorrectAnswers: ['Null type','Undefined type','Number type'],
-    },
-    {
-    question: 'The function and var/let are known as?',
-    correctAnswer: 'Declaration statements',
-    incorrectAnswers: ['Keywords','Data types','Prototypes'],
-    },
-    {
-    question: 'Which of the following statement is suitable to check if pattern matches the string text?',
-    correctAnswer: 'text==pattern',
-    incorrectAnswers: ['test(text)','equals(pattern)','test(pattern)'],
-    },
-    {
-    question: 'Which of the following is known as the equality operator, used to check whether two values are equal or not?',
-    correctAnswer: '==',
-    incorrectAnswers: ['=', '===', '&&'],
-    }
-]
+// let questions = [{
+//     question: 'Arrays in JS are defined by which of the following statements?',
+//     correctAnswer: 'Ordered list of values',
+//     incorrectAnswers: ['Ordered list of objects','Ordered list of strings','Ordered list of functions'],
+//     },
+//     {
+//     question: 'Which of the following is not js data types?',
+//     correctAnswer: 'All of the mentioned',
+//     incorrectAnswers: ['Null type','Undefined type','Number type'],
+//     },
+//     {
+//     question: 'The function and var/let are known as?',
+//     correctAnswer: 'Declaration statements',
+//     incorrectAnswers: ['Keywords','Data types','Prototypes'],
+//     },
+//     {
+//     question: 'Which of the following statement is suitable to check if pattern matches the string text?',
+//     correctAnswer: 'text==pattern',
+//     incorrectAnswers: ['test(text)','equals(pattern)','test(pattern)'],
+//     },
+//     {
+//     question: 'Which of the following is known as the equality operator, used to check whether two values are equal or not?',
+//     correctAnswer: '==',
+//     incorrectAnswers: ['=', '===', '&&'],
+//     }
+// ]
 
-questions.forEach(function (prompt) {
-}
-)
-let displayQuestion = document.querySelector(".header__format")
-displayQuestion.innerText = 
+// questions.forEach(function (prompt) {
+// }
+// )
+// let displayQuestion = document.querySelector(".header__format")
+// displayQuestion.innerText = 
 
 
 
-//This is the selector for the response options
-let response = document.getElementsByTagName("button").innerHTML = "this should show in the button";
-//console.log(response);
+// //This is the selector for the response options
+// let response = document.getElementsByTagName("button").innerHTML = "this should show in the button";
+// //console.log(response);
 
-//we need to creat a hidden tag with the question question array of object
-//then creat  a variabel in js to select and modify 
-let myMultiQuestions = document.querySelectorAll("div #button__container span").innerHTML = "This is an example question";
-//console.log(myMultiQuestions)
+// //we need to creat a hidden tag with the question question array of object
+// //then creat  a variabel in js to select and modify 
+// let myMultiQuestions = document.querySelectorAll("div #button__container span").innerHTML = "This is an example question";
+// //console.log(myMultiQuestions)
  
-
+let myFunction = () => {
+    console.log("this is working for the click");
+}
 
  question = ["question1", "question2", "question 3", "question4", "question 5"]
  function multipleChoiceQuestion(question) {
      nextQuestion = Math.floor(Math.random() * question.length) + 1;
-     return question[nextQuestion];
+     return nextQuestion
       }
- multipleChoiceQuestion(question);
+multipleChoiceQuestion(question);
+ 
+alert("this js is linked")
 //  //let anotherButton = document.querrySelector("#button__container")
 
  //document.getElementsByTagName(button).innerHTML =
